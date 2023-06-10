1d41162faddaf73d8c90452514cb8304ed7f773a
Robson Melo de Souza <sys.robson@gmail.com>
2023-06-10 20:32:28 -0300

    implemented the functionality

--- src/sections/Shortener/Shortener.js
+++ src/sections/Shortener/Shortener.js
@@ -1,35 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Shortener.scss";
-import Links from "./Links";
 
 const Shortener = () => {
+  let long = "";
+  let localStorageUrls = JSON.parse(localStorage.getItem("urls")) || [];
+  let [urls, setUrls] = useState(localStorageUrls);
+  const [isCopied, setIsCopied] = useState(false);
+
+  function updateLocalStorageUrls(urls) {
+    localStorage.setItem("urls", urls);
+    console.log("Local Storage Urls: ", localStorageUrls);
+  }
+
+  function handleLong(e) {
+    long = e.target.value;
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchUrl();
+  };
+
+  async function fetchUrl() {
+    try {
+      const response = await fetch("https://gotiny.cc/api", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ input: long }),
+      });
+      const jsonData = await response.json();
+      let urlsArray = urls.slice();
+      urlsArray.push(jsonData[0]);
+      console.log("JsonData", jsonData);
+      setUrls(urlsArray);
+      updateLocalStorageUrls(JSON.stringify(urlsArray));
+      console.log("StateUrls:", urls);
+    } catch (error) {
+      console.log("Error fetching: ", error);
+    }
+  }
+
+  function handleCopy(text, id) {
+    navigator.clipboard.writeText(text);
+    setIsCopied(id);
+  }
+
   return (
     <div className="Shortener">
-      <form className="Shortener__form">
+      <form className="Shortener__form" onSubmit={handleSubmit}>
         <input
           id="link"
           className="Shortener__input"
           type="text"
           placeholder="Shorten a link here"
           required
+          onChange={(e) => handleLong(e)}
         />
-        <button className="Shortener__button">Shorten it!</button>
+        <button className="Shortener__button" type="submit">
+          Shorten it!
+        </button>
       </form>
       <div className="Shortener__links">
-        {Links.map((link) => {
+        {urls.map((link) => {
+          const shortenedUrl = `https://gotiny.cc/${link?.code}`;
           return (
             <div className="Shortener__linkDisplay">
-              <a className="Shortener__originalLink" href={link.originUrl}>
-                {link.originUrl}
+              <a
+                className="Shortener__originalLink"
+                href={link?.originUrl}
+                target="_blank"
+              >
+                {link?.long}
               </a>
               <div className="Shortener__shortenedLinkDisplay">
                 <a
                   className="Shortener__shortenedLink"
-                  href={link.shortenedUrl}
+                  href={shortenedUrl}
+                  target="_blank"
                 >
-                  {link.shortenedUrl}
+                  {shortenedUrl}
                 </a>
-                <button className="Shortener__copyButton">Copy</button>
+                <button
+                  className="Shortener__copyButton"
+                  onClick={() => handleCopy(shortenedUrl)}
+                >
+                  {isCopied?.id === link.code ? "Copied" : "Copy"}
+                </button>
               </div>
             </div>
           );
